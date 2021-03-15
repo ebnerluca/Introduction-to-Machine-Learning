5920fbc5694c2de22f468466c47259e613dd2b65
PZchiron <pzhang@student.ethz.ch>
2021-03-15 13:06:14 +0100

    rmse calculated

--- task_1a/main.py
+++ task_1a/main.py
@@ -42,22 +42,40 @@ if __name__ == '__main__':
     # X = np.arange(50).reshape(10,5) #debug
 
     fold_size = int(len(X[:,1])/10.0)
+    error = np.array([])
     #print(fold_size)
 
     for i in range (len(alphas)):
+        error_temp = np.array([])
+
         for j in range(10):
             if j==0:
-                x_j = X[ fold_size: ,:]
+                x_j = X[ fold_size:, :]
+                x_left = X[0:fold_size, :]
+
                 y_j = y[ fold_size:]
+                y_left = y[0:fold_size]
             else:
                 x_j1 = X[ 0:j*fold_size , : ]
                 x_j2 = X[ (j+1)*fold_size:, : ]
                 x_j = np.vstack( (x_j1, x_j2) )
+                x_left = X[ j*fold_size:(j+1)*fold_size, :]
+
                 y_j1 = y[ 0:j*fold_size ]
                 y_j2 = y[ (j+1)*fold_size: ]
                 y_j = np.hstack( (y_j1, y_j2) )
+                y_left = y[ j*fold_size:(j+1)*fold_size ]
+
             #print(y_j)
-            training_model = Ridge(alpha = alphas[i])
+            training_model = Ridge(alpha = alphas[i]).fit(x_j,y_j)
+            y_predicted = training_model.predict(x_left)
+            error_temp = np.append(error_temp, [compute_RMSE(y_left, y_predicted)])
+            #score_j = training_model.score(x_j, y_j)
+            #print(score_j)
+        
+        error = np.append(error, [error_temp.mean()])
+        
+    print(error)            
             
            
 

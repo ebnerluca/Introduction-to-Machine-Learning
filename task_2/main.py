95bb408597a56a704bd7a919765eef251cbc57c9
Luca Ebner <ebnerl@ethz.com>
2021-04-19 11:09:02 +0200

    Added saving of preprocessed data.

--- task_2/main.py
+++ task_2/main.py
@@ -2,10 +2,10 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 from numpy import genfromtxt
 import math
+import pandas as pd
 
 
 def read_data(train_features_path, train_labels_path, test_features_path):
-
     print("Reading data...", end=" ", flush=True)
 
     train_features = np.genfromtxt(train_features_path, delimiter=",", skip_header=True)
@@ -17,15 +17,35 @@ def read_data(train_features_path, train_labels_path, test_features_path):
     return train_features, train_labels, test_features
 
 
-if __name__ == '__main__':
+def save_preprocessed_data(preprocessed_train_features):
+    print("Saving preprocessed data...", end=" ", flush=True)
 
+    df = pd.DataFrame(train_features,
+                      columns=["pid", "Time", "Age", "EtCO2", "PTT", "BUN", "Lactate", "Temp", "Hgb", "HCO3",
+                               "BaseExcess", "RRate", "Fibrinogen", "Phosphate", "WBC", "Creatinine", "PaCO2",
+                               "AST", "FiO2", "Platelets", "SaO2", "Glucose", "ABPm", "Magnesium", "Potassium",
+                               "ABPd", "Calcium", "Alkalinephos", "SpO2", "Bilirubin_direct", "Chloride",
+                               "Hct", "Heartrate", "Bilirubin_total", "TroponinI", "ABPs", "pH"])
+
+    df.to_csv("data/preprocessed/preprocessed_test_features.csv", index=False,header=True)
+
+    print("Done.")
+
+
+if __name__ == '__main__':
     train_features, train_labels, test_features = read_data("data/train_features.csv",
                                                             "data/train_labels.csv",
                                                             "data/test_features.csv")
-    
+
     print(f"shape of train_features: {train_features.shape}")
     print(f"shape of train_labels: {train_labels.shape}")
     print(f"shape of test_features: {test_features.shape}")
+
+    ## Preprocessing
+    # impute / delete missing information
+    # TODO
+    # save preprocessed data
+    save_preprocessed_data(train_features)
 
     ## SUBTASK 1: Ordering of medical test
 

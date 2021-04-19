90098f363b992b23d121e8c5143039364addf2cb
Luca Ebner <ebnerl@ethz.com>
2021-04-19 11:48:53 +0200

    Added time dimension reshaping for preprocessing.

--- task_2/main.py
+++ task_2/main.py
@@ -20,7 +20,7 @@ def read_data(train_features_path, train_labels_path, test_features_path):
 def save_preprocessed_data(preprocessed_train_features):
     print("Saving preprocessed data...", end=" ", flush=True)
 
-    df = pd.DataFrame(train_features,
+    df = pd.DataFrame(preprocessed_train_features,
                       columns=["pid", "Time", "Age", "EtCO2", "PTT", "BUN", "Lactate", "Temp", "Hgb", "HCO3",
                                "BaseExcess", "RRate", "Fibrinogen", "Phosphate", "WBC", "Creatinine", "PaCO2",
                                "AST", "FiO2", "Platelets", "SaO2", "Glucose", "ABPm", "Magnesium", "Potassium",
@@ -42,10 +42,18 @@ if __name__ == '__main__':
     print(f"shape of test_features: {test_features.shape}")
 
     ## Preprocessing
+    # reshape in dimensions (time, patients, values)
+    print("Preprocessing data...", end=" ", flush=True)
+    n_hours = 12
+    n_columns = train_features.shape[1] - 2  # minus 2 because pid is not relevant and we reshape in time dimension
+    n_rows = int(train_features.shape[0] / n_hours)
+    preprocessed_train_features = train_features[:, 2:].reshape((n_hours, n_rows, n_columns))
+    print("Done.")
+    print(f"shape of preprocessed_train_features: {preprocessed_train_features.shape}")
+
     # impute / delete missing information
-    # TODO
     # save preprocessed data
-    save_preprocessed_data(train_features)
+    # save_preprocessed_data(preprocessed_train_features)
 
     ## SUBTASK 1: Ordering of medical test
 

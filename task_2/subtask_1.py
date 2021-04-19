b8c57a2a150126448178e45ef129dfb5572f4408
Luca Ebner <ebnerl@ethz.com>
2021-04-19 16:47:03 +0200

    Bugfix for subtask 1

--- task_2/subtask_1.py
+++ task_2/subtask_1.py
@@ -19,28 +19,28 @@ class BinaryClassification(nn.Module):
         super(BinaryClassification, self).__init__()  # Number of input features is 5*35.
 
         n_inputs = 1*35
-        n_layer1 = 128
-        n_layer2 = 256
-        n_layer3 = 256
-        n_layer4 = 128
-        n_layer5 = 64
+        n_layer1 = 32
+        n_layer2 = 64
+        n_layer3 = 32
+        #n_layer4 = 64
+        #n_layer5 = 32
         n_outputs = 10
 
         self.layer_1 = nn.Linear(n_inputs, n_layer1)
         self.layer_2 = nn.Linear(n_layer1, n_layer2)
         self.layer_3 = nn.Linear(n_layer2, n_layer3)
-        self.layer_4 = nn.Linear(n_layer3, n_layer4)
-        self.layer_5 = nn.Linear(n_layer4, n_layer5)
+        #self.layer_4 = nn.Linear(n_layer3, n_layer4)
+        #self.layer_5 = nn.Linear(n_layer4, n_layer5)
 
-        self.layer_out = nn.Linear(n_layer5, n_outputs)
+        self.layer_out = nn.Linear(n_layer3, n_outputs)
 
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(p=0.1)
         self.batchnorm1 = nn.BatchNorm1d(n_layer1)
         self.batchnorm2 = nn.BatchNorm1d(n_layer2)
         self.batchnorm3 = nn.BatchNorm1d(n_layer3)
-        self.batchnorm4 = nn.BatchNorm1d(n_layer4)
-        self.batchnorm5 = nn.BatchNorm1d(n_layer5)
+        #self.batchnorm4 = nn.BatchNorm1d(n_layer4)
+        #self.batchnorm5 = nn.BatchNorm1d(n_layer5)
 
 
     def forward(self, inputs):
@@ -50,10 +50,10 @@ class BinaryClassification(nn.Module):
         x = self.batchnorm2(x)
         x = self.relu(self.layer_3(x))
         x = self.batchnorm3(x)
-        x = self.relu(self.layer_4(x))
+        """x = self.relu(self.layer_4(x))
         x = self.batchnorm4(x)
         x = self.relu(self.layer_5(x))
-        x = self.batchnorm5(x)
+        x = self.batchnorm5(x)"""
         x = self.dropout(x)
         x = self.layer_out(x)
 
@@ -99,31 +99,34 @@ def binary_acc(y_pred, y_test):
 if __name__ == '__main__':
 
     print("Reading data...", end=" ", flush=True)
-    train_data = np.genfromtxt("data/preprocessed/train_features_preprocessed_task1.csv", delimiter=",",
+    data = np.genfromtxt("data/preprocessed/train_features_preprocessed_task1.csv", delimiter=",",
                                skip_header=True)[:, 1:]
-    train_labels = np.genfromtxt("data/train_labels.csv", delimiter=",", skip_header=True)[:,1:11]
+    labels = np.genfromtxt("data/train_labels.csv", delimiter=",", skip_header=True)[:,1:11]
     print("Done.")
 
     #split train data in train and test set
-    train_data = train_data[18500:]
-    train_labels = train_labels[18500:]
-    test_data = train_data[:18500]
-    test_labels = train_data[:18500]
+    train_data = data[4000:,:]
+    train_labels = labels[4000:,:]
+    test_data = data[:4000,:]
+    test_labels = labels[:4000,:]
 
     print(f"shape of train_data: {train_data.shape}")
     print(f"shape of train_labels: {train_labels.shape}")
+    print(f"shape of test_data: {test_data.shape}")
+    print(f"shape of test_labels: {test_labels.shape}")
 
-    EPOCHS = 50
+
+    EPOCHS = 20
     BATCH_SIZE = 64
     LEARNING_RATE = 0.001
 
     train_data = TrainData(torch.FloatTensor(train_data), torch.FloatTensor(train_labels))
     minitest_data = TrainData(torch.FloatTensor(test_data), torch.FloatTensor(test_labels))
-    # test_data = TestData(torch.FloatTensor(X_test))
+    test_data = TestData(torch.FloatTensor(test_data))
 
     train_loader = DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
-    train_loader_test = DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
-    #test_loader = DataLoader(dataset=test_data, batch_size=BATCH_SIZE, shuffle=True)
+    train_loader_test = DataLoader(dataset=minitest_data, batch_size=BATCH_SIZE, shuffle=True)
+    # test_loader = DataLoader(dataset=test_data, batch_size=BATCH_SIZE, shuffle=True)
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = BinaryClassification()
@@ -159,26 +162,26 @@ if __name__ == '__main__':
             epoch_acc += acc.item()
 
         # print(f'Epoch {e + 0:03}: | Loss: {epoch_loss / len(train_loader):.5f} 'f'| Acc: {epoch_acc / len(train_loader):.3f}')
+
         model.eval()
         test_epoch_acc = 0
-        for X_batch, y_batch in train_loader_test:
+        for X_batch_test, y_batch_test in train_loader_test:
 
-            X_batch, y_batch = X_batch.to(device), y_batch.to(device)
+            X_batch_test, y_batch_test = X_batch_test.to(device), y_batch_test.to(device)
 
-            y_pred = model(X_batch)
+            y_pred_test = model(X_batch_test)
 
-            y_pred = y_pred.reshape(-1,1)
-            y_batch = y_batch.reshape(-1,1)
+            y_pred_test = y_pred_test.reshape(-1,1)
+            y_batch_test = y_batch_test.reshape(-1,1)
 
-            acc = binary_acc(y_pred, y_batch)
+            acc_test = binary_acc(y_pred_test, y_batch_test)
 
-            test_epoch_acc += acc.item()
+            test_epoch_acc += acc_test.item()
         # print(f'Epoch {e + 0:03}: | Test Acc: {epoch_acc / len(train_loader_test):.3f}')
         model.train()
 
         print(f'Epoch {e + 0:03}: | Loss: {epoch_loss / len(train_loader):.5f} 'f'| Acc: '
               f'{epoch_acc / len(train_loader):.3f} 'f'| Test Acc: {test_epoch_acc / len(train_loader_test)}')
-
 
 
     ## SUBTASK 2: Sepsis prediction

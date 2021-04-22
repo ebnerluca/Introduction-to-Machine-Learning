53e07294659bcd8b49531cd48ee72480cbc7d21c
PZchiron <pzhang@student.ethz.ch>
2021-04-22 10:53:06 +0200

    final subm

--- task_2/subtask_3_new.py
+++ task_2/subtask_3_new.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 from sklearn.model_selection import cross_val_score
 from sklearn.experimental import enable_hist_gradient_boosting
-from sklearn.ensemble import HistGradientBoostingClassifier
+from sklearn.ensemble import HistGradientBoostingRegressor
 import sklearn.metrics as metrics
 
 import torch
@@ -80,8 +80,8 @@ if __name__ == '__main__':
 
     for i in range(4):
 
-        label = np.expand_dims(labels[:,i], axis=1)
-
+        # NN approach
+        '''label = np.expand_dims(labels[:,i], axis=1)
         regressor = NeuralNetRegressor(
             RegressionNetwork,
             criterion=nn.MSELoss,
@@ -89,7 +89,10 @@ if __name__ == '__main__':
             train_split=dataset.CVSplit(5, stratified=False), 
             max_epochs=5,
             lr=0.015,
-        )
+        )'''
+        # Histo approach
+        label = labels[:,i]
+        regressor = HistGradientBoostingRegressor(max_depth=3)
 
         ### calculate cross validation score for training mode
         if training_mode:
@@ -98,6 +101,7 @@ if __name__ == '__main__':
             hand_in_metric = 0.5 + 0.5 * np.maximum(0, scores.mean())
             print("Cross-Validation score {score:.3f},"
                   .format(score = hand_in_metric))
+
             mean_score += hand_in_metric
 
         ### generate output if not
@@ -106,7 +110,9 @@ if __name__ == '__main__':
             predictions = regressor.predict(test)
             training_score = 0.5 + 0.5 * np.maximum(0, metrics.r2_score(label, regressor.predict(data)))
             print("Training score:", training_score)
-            output_array = np.hstack((output_array, predictions))
+            #print(predictions.reshape(-1,1).shape)
+            #print(output_array.shape)
+            output_array = np.hstack((output_array, predictions.reshape((-1,1)) ))
 
     if training_mode:
         print(f"Hand in metric score is: {mean_score/4}")

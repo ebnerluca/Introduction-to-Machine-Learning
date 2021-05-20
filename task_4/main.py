31b703b9b03ade76e0781a1f8adba6486feba48a
Luca Ebner <ebnerl@ethz.com>
2021-05-20 10:40:13 +0200

    Feature computation via pretrained classifier can now be run via GPU.

--- task_4/main.py
+++ task_4/main.py
@@ -6,12 +6,17 @@ from torchvision import datasets, transforms
 # import matplotlib.pyplot as plt
 
 
-# config
+#### Config ####
 n_images = 10000
 img_size = (224, 224)
-image_loader_batch_size = 4
+image_loader_batch_size = 32
 triplet_loader_batch_size = 32
 encoder_features = 1000  # dependant on output of classifier
+
+compute_features = True
+features_path = "data/features.txt"
+################
+
 
 # Read Data
 transform = transforms.Compose([transforms.Resize(img_size),
@@ -26,37 +31,33 @@ print(f"triplets shape: {triplets.shape}")
 imageloader = torch.utils.data.DataLoader(images, batch_size=image_loader_batch_size, shuffle=False)
 tripletloader = torch.utils.data.DataLoader(triplets, batch_size=triplet_loader_batch_size, shuffle=False)
 
-# Model
-classifier_model = torchvision.models.mobilenet_v3_large(pretrained=True, progress=True)
+# Model for feature predictions
+classifier_model = torchvision.models.mobilenet_v3_small(pretrained=True, progress=True)
 classifier_model.eval()
-"""device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model.to(device)
-print(f"Device: {device}")"""
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+classifier_model.to(device)
+print(f"Device: {device}")
 
-# Classification prediction
-# predictions = np.zeros((n_images, encoder_features))
-"""for image_batch in imageloader:
-
+# Compute features by using pretrained model
+if compute_features:
+    features = np.zeros((0, encoder_features))
     iter = 0
-    for image in image_batch:
-        print(f"type of image: {type(image)}")
-        print(f"iter: {iter}")
-        print(f"type of image_batch = {type(image)}")
-        prediction = classifier_model(image)
-        print(f"prediction.shape = {prediction.shape}")
-        iter = iter + 1"""
+    for image_batch in imageloader:
+        print(f"Computing features... {iter} / {n_images}", end="\r")#, flush=True)
+        image_batch = image_batch[0]  # get rid of target classes
+        image_batch = image_batch.to(device)
+        features_batch = classifier_model(image_batch)
+        features = np.vstack((features, features_batch.cpu().detach().numpy()))
+        iter = iter + image_loader_batch_size
+    print(f"\nComputing features done. Saving features under {str(features_path)}")
+    np.savetxt(features_path, features)
+else:
+    print("Loading features...")
+    features = np.loadtxt(features_path)
+    print("Loading features done.")
 
-for image in images:
-    print(image[0])
-    prediction = classifier_model(image[0])
+print("All finished")
 
-print("finished")
-
-
-"""images, labels = next(iter(dataloader))
-# plt.imshow(np.squeeze(images[0].permute(1,2,0)))
-plt.imshow(images[0])
-plt.show()"""
 
 
 
